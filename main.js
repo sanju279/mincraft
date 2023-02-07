e92fd3fa6ddfb92d9f951fddb04d430c5f50b3d4
Sanju279 <117310661+sanju279@users.noreply.github.com>
2023-02-07 18:10:00 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,136 @@
+// Create the reference variable of the canvas using fabric.Canvas()
+var canvas = new fabric.Canvas('myCanvas');
+
+// Define the starting width and height of the block images
+block_width = 30;
+block_height = 30;
+
+// Define the starting x and y coordinates for the player
+player_x = 10;
+player_y = 10;
+
+// Define a variable named player_object to store object of the player image
+var player_object =""; 
+var block_object ="";
+// Add a function named player_update() to add the player image
+function player_update()
+{
+fabric.Image.fromURL("player.png", function(img){
+player_object = img;
+
+player_object.scaleToWidth(150);
+player_object.scaleToHeight(140);
+player_object.set({
+top : player_y,
+left : player_x
+});
+canvas.add(player_object);
+});
+
+}
+// Add a function named new_image() to add the different images as per the specific keys pressed
+function new_image(get_image)
+{
+fabric.Image.fromURL(get_image, function(img){
+block_object = img;
+
+block_object.scaleToWidth(block_width);
+block_object.scaleToHeight(block_height);
+block_object.set({
+top : player_y,
+left : player_x
+});
+canvas.add(block_object);
+});
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if(e.shiftKey == true && keyPressed =='80')
+    {
+        console.log("p and shift pressed together");
+        block_width = block_width +10;
+        block_height = block_height +10;
+        document.getElementById("current_width").innerHTML = block_width;
+        document.getElementById("current_height").innerHTML = block_height;
+    }
+
+    if(e.shiftKey && keyPressed =='77')
+    {
+        console.log("m and shift pressed together");
+        block_width = block_width -10;
+        block_height = block_height -10;
+        document.getElementById("current_width").innerHTML = block_width;
+        document.getElementById("current_height").innerHTML = block_height;
+    }
+
+    if(keyPressed == '38')
+    {
+        up();
+        console.log("up");
+    }
+    if(keyPressed == '40')
+    {
+        down();
+        console.log("down");
+    }
+    if(keyPressed == '37')
+    {
+        left();
+        console.log("left");
+    }
+    if(keyPressed == '39')
+    {
+        right();
+        console.log("right");
+    }
+    if(keyPressed == '87')
+    {
+        new_img('wall.jpg');
+        console.log("w");
+    }
+    if(keyPressed == '71')
+    {
+        new_img('ground.png');
+        console.log("g");
+    }
+    if(keyPressed == '76')
+    {
+        new_img('light_green.png');
+        console.log("l");
+    }
+    if(keyPressed == '84')
+    {
+        new_img('trunk.jpg');
+        console.log("t");
+    }
+    if(keyPressed == '82')
+    {
+        new_img('roof.jpg');
+        console.log("r");
+    }
+    if(keyPressed == '89')
+    {
+        new_img('yellow_wall.png');
+        console.log("y");
+    }
+    if(keyPressed == '68')
+    {
+        new_img('dark_green.png');
+        console.log("d");
+    }
+    if(keyPressed == '85')
+    {
+        new_img('unique.png');
+        console.log("u");
+    }
+    if(keyPressed == '67')
+    {
+        new_img('cloud.jpg');
+        console.log("c");
+    }
+}
+}
